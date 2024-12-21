18242b9cf9d089279caa7f7d1a5c06d3200b3012
Rostyk <137609191+Roctsk@users.noreply.github.com>
2024-12-21 13:36:03 +0300

    Add files via upload

---
+++ Roctsk/script.js
@@ -0,0 +1,3 @@
+        <img class="img_music" style="width:20px;,height:20px;" src="img_2.png">
+        <img style="width:20px;,height:20px;" src="img_3.png">
+        <img style="width:20px;,height:20px;" src="img_4.png">
